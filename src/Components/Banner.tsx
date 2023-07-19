10bf8fd1c506e1ea5c88f77f92d860338e871359
Flávio Bulhões <flavio.profissional1@gmail.com>
2023-07-19 10:20:20 -0300

    teste vercel

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -92,7 +92,7 @@ export function Banner(props: MainProps) {
       </div>
       <h3>
         Conheça as estratégias que <b>mudaram o jogo</b> e como aplicá-las no
-        seu negócio
+        seu negócião
       </h3>
     </MainLeadSection>
   );
